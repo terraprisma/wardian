30803d74db31cb4e85ecb7dcf78e72f02187c3b2
tomat <xxlennygamerxx@gmail.com>
2024-01-14 03:40:03 -0800

    Use System.Numerics.

---
+++ patches/Wardian/Terraria/Usings.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * global using Vector2 = System.Numerics.Vector2;
+ * global using Vector3 = System.Numerics.Vector3;
+ * global using Vector4 = System.Numerics.Vector4;
+ */
+global using System.Numerics;
+global using Matrix = System.Numerics.Matrix4x4;
+global using static Microsoft.Xna.Framework.NumericsExtensions;
